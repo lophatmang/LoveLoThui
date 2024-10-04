79aa79e498729af2728d5040f75ac784e7d2ffb2
LoPhatMang <thanhntfx32690@funix.edu.vn>
2024-10-05 05:27:18 +0700

    a

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-async function clickAction() {
+function clickAction() {
   document.querySelector(".vehicle-body").classList.add("clickAction");
-  await setTimeout(console.log(), 4000);
-  document.querySelector(".vehicle-body").innerHTML = "";
 }
